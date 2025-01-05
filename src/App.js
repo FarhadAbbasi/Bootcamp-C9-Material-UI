5a18e0d67639592998975075eb687007c7dbefbf
Farhad Ali Abbasi <34246950+FarhadAbbasi@users.noreply.github.com>
2025-01-05 12:26:08 +0500

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,136 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Button, Grid2, ImageList, ImageListItem } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import ResponsiveAppBar from './Components/AppBar';
+
+
 function App() {
   return (
     <div>
-      Hello Material UI
+      <h1>Hello Material UI</h1> <br /><br />
+      <ResponsiveAppBar/><br /> 
+      <Button variant='contained' color='secondary'>  I'm just a Button   </Button>
+      <Button variant='outlined' color='secondary' size='small'>  Don't Click Me   </Button> <br /><br />
+
+      <Grid2 container rowSpacing={8} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid2>
+         Nothing
+          <Paper elevation={1} style={{padding:"10px"}}> It's a Paper Component </Paper>
+        </Grid2>
+        <Grid2>
+          <Paper />
+          <Paper elevation={3} />
+         Nothing
+          <Paper elevation={1} style={{padding:"20px"}}> It's a Paper Component </Paper>
+        </Grid2>
+        <Grid2>
+          Nothing
+          <Paper elevation={1} style={{padding:"50px"}} > It can also be a Card </Paper>
+          </Grid2>
+      </Grid2>
+
+      <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+        {itemData.map((item) => (
+          <ImageListItem key={item.img}>
+            <img
+              srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+              src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+              alt={item.title}
+              loading="lazy"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
+
+      {/* <ImageList
+        sx={{ width: 500, height: 450 }}
+        variant="quilted"
+        cols={4}
+        rowHeight={121}
+      >
+        {itemData.map((item) => (
+          <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
+            <img
+              {...srcset(item.img, 121, item.rows, item.cols)}
+              alt={item.title}
+              loading="lazy"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList> */}
+
+
     </div>
   );
 }
 
+
+
+
+function srcset(image, size, rows = 1, cols = 1) {
+  return {
+    src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
+    srcSet: `${image}?w=${size * cols}&h=${size * rows
+      }&fit=crop&auto=format&dpr=2 2x`,
+  };
+}
+
+
 export default App;
+
+
+const itemData = [
+  {
+    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+    title: 'Breakfast',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+    title: 'Burger',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+    title: 'Camera',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+    title: 'Coffee',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+    title: 'Hats',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+    title: 'Honey',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+    title: 'Basketball',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+    title: 'Fern',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+    title: 'Mushrooms',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+    title: 'Tomato basil',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+    title: 'Sea star',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+    title: 'Bike',
+  },
+];
